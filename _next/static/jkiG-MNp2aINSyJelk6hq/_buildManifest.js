504f90c7164be366692674f659b92a211d07832a
mahafujul <mahafujul@users.noreply.github.com>
2026-01-09 02:45:51 +0000

    deploy: d0f18e7ccf162adb6a72c09c7cdc80564141f4fd

---
+++ _next/static/jkiG-MNp2aINSyJelk6hq/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/git-commands-hub//_next/:path+",
+        "destination": "/git-commands-hub/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
